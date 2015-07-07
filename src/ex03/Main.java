f73d3f53aed230d903516af28369b6b625136f3e
ricardo <ricardo.rsr.rodrigues@gmail.com>
2015-07-07 10:46:40 +0100

    ex 04

---
+++ src/ex03/Main.java
@@ -0,0 +1,21 @@
+package ex03;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] points = new int[]{100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+		
+		// sequential
+				System.out.println("Sequential");
+				for (int i : points) {
+					System.out.println(i);
+					Sequential.main(new String[]{String.valueOf(i)});
+				}
+				
+				// parallel
+				System.out.println("Parallel");
+				for (int i : points) {
+					System.out.println(i);
+					Parallel.main(new String[]{String.valueOf(i)});
+				}
+	}
+}
---
+++ src/ex04/Server.java
@@ -0,0 +1,63 @@
+package ex04;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+	private static final int NUM_THREADS = 4;
+	ArrayBlockingQueue<Double> queue = new ArrayBlockingQueue<>(10);
+	
+	public void add(double x) {
+		queue.add(x);
+	}
+	
+	public Double get() {
+		try {
+			return queue.take();
+		} catch (InterruptedException e) {}
+		return null;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		final Server server = new Server();
+		ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
+		
+		// master thread
+		pool.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				Random rand = new Random();
+				int max_number = 100;
+				
+				while (true) {
+					server.add(rand.nextDouble()*max_number);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {}
+				}
+			}
+			
+		});
+		
+		// worker threads
+		for (int i = 0; i < NUM_THREADS-1; i++) {
+			pool.submit(new Runnable() {
+
+				@Override
+				public void run() {
+					while (true) {
+						double n = server.get();
+						System.out.println("Thread "+Thread.currentThread().getId()+" -> square root of "+n+" is "+Math.sqrt(n));
+					}
+				}
+				
+			});
+		}
+	}
+}
